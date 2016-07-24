a7c740c7df360abf62c398ee5d6f59e275fcb27f
vargi <tunavargi@gmail.com>
2016-07-24 20:37:40 +0200

    hate simulation

--- ajan/behaviors.py
+++ ajan/behaviors.py
@@ -8,8 +8,12 @@ def increase_hate(agent, model):
 
 def racism(agent, model):
     if agent.nationalism > 20:
-        print("hello racism")
+        cellmates = model.grid.get_cell_list_contents([agent.pos])
+        map(model.schedule.remove, cellmates)
+        model.schedule.remove(agent)
+        print('boom')
     else:
+        print('increase')
         increase_hate(agent, model)
 
 
