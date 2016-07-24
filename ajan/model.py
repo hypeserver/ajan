fd86103c5cb207fcbd098a464fd69981c3c1168e
umitdincel <dincelumit@gmail.com>
2016-07-24 20:20:57 +0300

    Fixed calculation

--- ajan/model.py
+++ ajan/model.py
@@ -14,7 +14,7 @@ class WorldModel(Model):
         for i in range(self.num_agents):
             x = random.randrange(self.grid.width)
             y = random.randrange(self.grid.height)
-            ethnicity = (((x+1) * (y+1)) / self.grid.width * self.grid.height) * len(Ethnicities)
+            ethnicity = ((x+1) * (y+1)) / float(self.grid.width * self.grid.height) * len(Ethnicities)
             a = PersonAgent(i, ethnicty=int(ethnicity))
             self.schedule.add(a)
             # Add the agent to a random grid cell
