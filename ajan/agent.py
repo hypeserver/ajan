d78c48479ca1785721f240b9f62b4c0fb09646e1
yigit <yigit.guneli@publik.com.tr>
2016-07-27 20:02:21 +0300

    agents does not get removed from the grid if they are dead. also they now move only to nearby empty cells.

--- ajan/agent.py
+++ ajan/agent.py
@@ -1,31 +1,38 @@
+import random
 
-from mesa import  Agent
-import  random
+from mesa import Agent
+
 from behaviors import racism
 
-Ethnicities = [i for i in range(10)]
-class PersonAgent(Agent):
+Ethnicities = [i for i in range(4)]
 
+
+class PersonAgent(Agent):
     behaviors = [racism]
 
     def __init__(self, **kwargs):
         self.nationalism = random.randint(1,20)
         self.ethnicity = kwargs.get('ethnicity')
-        self.unique_id = kwargs.get('unique_id')
+        self.is_living = True
+        super().__init__(kwargs.get('unique_id'), kwargs.get('model'))
 
-    def move(self, model):
-        possible_steps = model.grid.get_neighborhood(
-            self.pos,
-            moore=True,
-            include_center=False)
-        new_position = random.choice(possible_steps)
-        model.grid.move_agent(self, new_position)
+    def move(self):
+        self.model.grid.move_to_empty(self)
 
+    def move_to_nearby_empty(self):
+        neighborhood = self.model.grid.get_neighborhood(self.pos, moore=True, radius=1)
+        nearby_empty_cells = [nearby_cell for nearby_cell in neighborhood if self.model.grid.is_cell_empty(nearby_cell)]
 
-    def step(self, model):
-        if self.nationalism > 50:
-            self.move(model)
+        if nearby_empty_cells:
+            self.model.grid.move_agent(self, random.choice(nearby_empty_cells))
 
-        for behaviour in self.behaviors:
-            behaviour(self, model)
+    def step(self):
+        if self.is_living:
+            if self.nationalism > 20:
+                self.move_to_nearby_empty()
 
+            for behaviour in self.behaviors:
+                behaviour(self, self.model)
+
+    def kill(self):
+        self.is_living = False
--- ajan/behaviors.py
+++ ajan/behaviors.py
@@ -1,26 +1,24 @@
-
-
 def calculate_hate(agent, model):
-    cellmates = model.grid.get_cell_list_contents([agent.pos])
-    cellmates_hated = [cellmate for cellmate in cellmates if cellmate.ethnicity != agent.ethnicity]
+    cellmates = model.grid.get_neighbors(agent.pos, moore=True, radius=1)
+    cellmates_hated = [cellmate for cellmate in cellmates if cellmate.ethnicity != agent.ethnicity and cellmate.is_living]
     
     if len(cellmates_hated):
         agent.nationalism += len(cellmates_hated)
     else:
-        agent.nationalism -= 1
-
+        agent.nationalism -= 9 - len(cellmates_hated)
 
 
 def racism(agent, model):
-
     calculate_hate(agent, model)
 
     if agent.nationalism > 40:
-        cellmates = model.grid.get_cell_list_contents([agent.pos])
-        map(model.schedule.remove, cellmates)
-        model.schedule.remove(agent)
-        #model.grid._remove_agent(agent.pos,agent)
+        neighbours = model.grid.get_neighborhood(
+            agent.pos,
+            moore=True,
+            include_center=True,
+            radius=3)
 
-        for i in cellmates:
-            model.grid._remove_agent(i.pos, i)
+        cellmates = model.grid.get_cell_list_contents(neighbours)
 
+        for agent in cellmates:
+            agent.kill()
