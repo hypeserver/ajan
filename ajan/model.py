755b575a62f7904abc2e3e18a518341aff1986c4
yigit <yigit.guneli@publik.com.tr>
2016-07-27 20:01:03 +0300

    Switching to single grid to better visualize the world

--- ajan/model.py
+++ ajan/model.py
@@ -1,30 +1,31 @@
+import random
+
 from mesa import Model
 from mesa.time import RandomActivation
-from mesa.space import MultiGrid
+from mesa.space import SingleGrid
 from mesa.datacollection import DataCollector
 
 from agent import PersonAgent, Ethnicities
 
-import  random
 
 class WorldModel(Model):
     def __init__(self, N, width, height):
-        self.grid = MultiGrid(height, width, True)
+        self.grid = SingleGrid(height, width, True)
 
         self.schedule = RandomActivation(self)
         self.num_agents = N
         self.running = True
         
-        
         for i in range(self.num_agents):
-            x = random.randrange(self.grid.width)
-            y = random.randrange(self.grid.height)
             ethnicity = random.choice(Ethnicities)
             a = PersonAgent(unique_id=i,
-                            ethnicity=int(ethnicity))
+                            model=self,
+                            ethnicity=int(ethnicity)
+                            )
             self.schedule.add(a)
             # Add the agent to a random grid cell
-            self.grid.place_agent(a, (x, y))
+
+            self.grid.position_agent(a)
             
         self.datacollector = DataCollector(
             agent_reporters={
