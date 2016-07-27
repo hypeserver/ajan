89daeed884a8647eaa128e2af389616d5df10ece
yigit <yigit.guneli@publik.com.tr>
2016-07-27 20:00:32 +0300

    Dead persons are represented with black colour

--- ajan/visualize.py
+++ ajan/visualize.py
@@ -1,22 +1,27 @@
 from mesa.visualization.modules import CanvasGrid
 from mesa.visualization.ModularVisualization import ModularServer
 
-
 from model import WorldModel
 
+
 def agent_portrayal(agent):
-    ethnicity_colors = ["black", "blue", "red", "green", "yellow", "purple", "pink", "orange", "brown", "grey", "magenta"]
+    ethnicity_colors = ["red", "green", "yellow", "purple", "pink", "orange", "brown", "grey", "magenta"]
     portrayal = {"Shape": "circle",
                  "Filled": "true",
                  "Layer": 1,
-                 "r": 0.5}
+                 "r": 0.5,
+                 "Color": ethnicity_colors[agent.ethnicity]
+                 }
     
-    if agent.nationalism > 20:
-        portrayal["Layer"] = 1
+    if agent.nationalism > 25:
+        portrayal["Layer"] = 0
         portrayal["r"] = 1.5
 
-    portrayal['Color'] = ethnicity_colors[agent.ethnicity]
-
+    if not agent.is_living:
+        portrayal["text"] = "X"
+        portrayal["text_color"] = "black"
+        portrayal["Layer"] = 2
+        portrayal["Color"] = "black"
 
     return portrayal
 
@@ -24,7 +29,7 @@ grid = CanvasGrid(agent_portrayal, 50, 50, 1000, 1000)
 
 server = ModularServer(WorldModel,
                        [grid],
-                       "World Model",
-                       800, 50, 50)
+                       "Canli Bomba",
+                       400, 50, 50)
 server.port = 8889
 server.launch()
